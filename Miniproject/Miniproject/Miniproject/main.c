7f23e28626e41ebd6bc7545f83facb4c153da84e
DionBartelen <dionbartelen@gmail.com>
2018-03-08 17:08:01 +0100

    Added a folder for the Miniproject

---
+++ Miniproject/Miniproject/Miniproject/main.c
@@ -0,0 +1,18 @@
+/*
+ * Miniproject.c
+ *
+ * Created: 8-3-2018 14:33:50
+ * Author : dionb
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
